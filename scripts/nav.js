7d92f3895abfa0f52cf018ec9d9efdf524ecb985
Sarah Lord <113374720+slord30@users.noreply.github.com>
2025-07-09 16:51:19 -0500

    first commit

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,7 +1,7 @@
 //
 //    Toggle Mobile Navigation
 //
-const navigation = document.querySelector('#nav')
+const navigation = document.querySelector('.navigation');
 const hamButton = document.querySelector('#menu');
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open')
